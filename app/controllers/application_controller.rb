65d371e8ed5d3fa402a905a389861c4cc72cac2f
Łukasz Draba <hybris@o2.pl>
2011-06-15 12:19:58 +0200

    language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,13 @@ class ApplicationController < ActionController::Base
 
   before_filter :set_locale
 
-  def default_url_options(options={})
+  def default_url_options(options = {})
     { :locale => I18n.locale }
   end
 
   private
 
   def set_locale
-    I18n.locale = params[:locale] if params.include? 'locale' and I18n.available_locales.include? params[:locale].to_sym || I18n.default_locale
+    I18n.locale = params[:locale] if params.member? 'locale' and I18n.available_locales.member? params[:locale].to_sym || I18n.default_locale
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,27 @@
 module ApplicationHelper
-  def app_name
+  def title
     I18n.t("app_name")
+  end
+
+  def language_switcher(options = {})
+    opts = {:language_switcher_class => 'language_switcher', :language_current_class => 'language_current'}
+    opts.merge! options
+    content_tag :div, :class => opts[:language_switcher_class] do
+      content_tag :ul do
+        I18n.available_locales.sort.collect do |locale|
+          concat(content_tag :li, language_link(locale), :class => (opts[:language_current_class] if language_current?(locale)))
+        end
+      end
+    end
+  end
+
+  private
+
+  def language_link(locale)
+    link_to(image_tag("language/#{locale}.png", :alt => I18n.t(locale, :scope => "language")), :locale => locale)
+  end
+
+  def language_current?(locale)
+    locale == I18n.locale
   end
 end
